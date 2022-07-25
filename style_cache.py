1054a56947eec6b6df5e601347be7ffe0c7c2bb1
theRealProHacker <77074862+theRealProHacker@users.noreply.github.com>
2022-07-25 20:00:59 +0200

    Weakref in the style_cache with a test

--- style_cache.py
+++ style_cache.py
@@ -1,10 +1,14 @@
 from copy import copy
-from types import MappingProxyType as frozendict
+from weakref import WeakValueDictionary
+from frozendict import frozendict as _frozendict
+
+class frozendict(_frozendict): # to make the weakref work
+    pass
 
 from own_types import computed_value, style_computed
 
 _Key = tuple[tuple[str, computed_value], ...]
-cache: dict[_Key, style_computed] = {}
+cache: WeakValueDictionary[_Key, style_computed] = WeakValueDictionary()
 
 def get_key(d: style_computed)->_Key:
     return (*sorted(d.items(), key = lambda x: x[0]),)
@@ -13,5 +17,33 @@ def safe_style(d: style_computed)->style_computed:
     d = copy(d)
     key = get_key(d)
     if key not in cache:
-        cache[key] = frozendict(d)
+        frz = frozendict(d) # we need to hold this reference until we return
+        cache[key] = frz
     return cache[key]
+
+
+def test():
+    style1 = {
+        "display": "block",
+        "margin-top": "1em",
+        "margin-bottom": "1em",
+    }
+    style2 = {
+        "margin-bottom": "1em",
+        "margin-top": "1em",
+        "display": "block",
+    }
+    # 1. Insert dict works
+    style3 = safe_style(style1)
+    assert len(cache) == 1
+
+    # 2. when adding another element the cache doesn't grow
+    style4 = safe_style(style2)
+    assert len(cache) == 1
+    # 3. and returns the found value
+    style3 is style4
+
+    # 4. when we delete the reference to the cache item, the cache gets cleared
+    style3 = None
+    del style4
+    assert not cache
