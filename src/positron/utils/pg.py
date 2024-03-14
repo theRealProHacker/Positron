1c13e31d84dfb8cda10204bd51c764376341eaf3
theRealProHacker <77074862+theRealProHacker@users.noreply.github.com>
2024-03-14 04:29:41 +0200

    Added draw transparent to pg util

--- src/positron/utils/pg.py
+++ src/positron/utils/pg.py
@@ -5,6 +5,8 @@ This module should slowly be dissolved into utils sub modules
 """
 
 from contextlib import contextmanager
+from functools import wraps
+from typing import Sequence
 import numpy as np
 import pygame as pg
 
@@ -32,6 +34,39 @@ def get_rect(rect: Rect, color: ColorValue, **kwargs):
     if color.a != 255:
         drawn_rect.set_alpha(color.a)
     return drawn_rect
+
+
+def _draw_transparent(func):
+    @wraps(func)
+    def inner(surf: Surface, *args, **kwargs):
+        drawn = Surface(surf.get_size(), pg.SRCALPHA)
+        drawn.fill("transparent")
+        func(drawn, *args, **kwargs)
+        surf.blit(drawn, (0, 0))
+
+    return inner
+
+
+draw_polygon = _draw_transparent(pg.draw.polygon)
+"""
+Draw a polygon onto the surf
+
+surf,
+color,
+points,
+width
+"""
+
+draw_line = _draw_transparent(pg.draw.line)
+"""
+Draw a line onto the surf
+
+surface: Surface,
+color: ColorValue,
+start_pos: Coordinate,
+end_pos: Coordinate,
+width: int = 1
+"""
 
 
 def surf_opaque(surf: Surface) -> bool:
